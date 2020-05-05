7bde2267f0399801f75b192a43c915d746613e06
yilpe <yilpe@naver.com>
2020-05-05 22:46:50 +0900

    배포 준비

--- server/app.js
+++ server/app.js
@@ -56,12 +56,12 @@ if (prod) {
 app.use("/", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookie("process.env.COOKIE_SECRET"));
+app.use(cookie(process.env.COOKIE_SECRET));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    secret: "process.env.COOKIE_SECRET",
+    secret: process.env.COOKIE_SECRET,
     cookie: {
       httpOnly: true,
       secure: false,
