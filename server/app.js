ec6ef3c8585097e189181f11b3dd4e191cb62486
yilpe <yilpe@naver.com>
2020-05-05 22:14:48 +0900

    배포 준비

--- server/app.js
+++ server/app.js
@@ -4,7 +4,13 @@ const passport = require("passport");
 const session = require("express-session");
 const cookie = require("cookie-parser");
 const morgan = require("morgan");
+const hpp = require("hpp");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
 
+const prod = process.env.NODE_ENV === "production";
+
+dotenv.config();
 const db = require("./models");
 // Passport Config
 const passportConfig = require("./passport");
@@ -22,31 +28,44 @@ db.sequelize.sync();
 passportConfig();
 
 /* Middleware */
-app.use(morgan("dev"));
-/* 
-  # cols 관련 HTTP Header
-  Access-Control-Allow-Origin
-  Access-Control-Allow-Credentials
-*/
-app.use(
-  cors({
-    origin: "http://localhost:3080",
-    credentials: true,
-  })
-);
+if (prod) {
+  app.use(helmet()); // 보안,
+  app.use(hpp()); // 보안,
+  app.use(morgan("combined"));
+  app.use(
+    cors({
+      origin: "http://vue-twitter.com",
+      credentials: true,
+    })
+  );
+} else {
+  app.use(morgan("dev"));
+  /* 
+    # cols 관련 HTTP Header
+    Access-Control-Allow-Origin
+    Access-Control-Allow-Credentials
+  */
+  app.use(
+    cors({
+      origin: "http://localhost:3080",
+      credentials: true,
+    })
+  );
+}
 // 정적파일 제공해주기 위해 (프론트 접근 url, 백 제공 폴더) => 서버 주소와 프론트 주소는 웬만하면 다르게, 백의 주소 노출 위험
 app.use("/", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookie("cookiesecret"));
+app.use(cookie("process.env.COOKIE_SECRET"));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    secret: "cookiesecret",
+    secret: "process.env.COOKIE_SECRET",
     cookie: {
       httpOnly: true,
       secure: false,
+      domain: prod && ".vue-twitter.com",
     },
   })
 );
@@ -58,6 +77,8 @@ app.use("/post", postRouter);
 app.use("/posts", postsRouter);
 app.use("/hashtag", hashtagRouter);
 
-app.listen(3085, () => {
-  console.log(`백엔드 서버 ${3085}번 포트에서 작동중..`);
+app.listen(prod ? process.env.PORT : 3085, () => {
+  console.log(
+    `백엔드 서버 ${prod ? process.env.PORT : 3085}번 포트에서 작동중..`
+  );
 });
